7add68c5e818fef590f588c885a0ec2981597dcd
Motjiang <motjiangbrilliant@gmail.com>
2024-12-10 03:04:39 +0200

    Added category model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CatgoryAddComponent } from './components/catgory-add/catgory-add.component';
+import { CategoryAddComponent } from './components/category-add/category-add.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { CategoryUpdateComponent } from './components/category-update/category-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    CatgoryAddComponent,
+    CategoryAddComponent,
+    CategoryListComponent,
+    CategoryUpdateComponent
   ],
   imports: [
     BrowserModule,
